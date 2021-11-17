a113f2c5ff8cd6be431d8c3aa8c1ec11c4476c82
Android2021 <jezreldavesondia@gmail.com>
2021-11-17 20:55:22 +0800

    added cache

--- api/index.js
+++ api/index.js
@@ -21,6 +21,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     next();  
 })
 
