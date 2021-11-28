526df15897b9dc336d77dbee2e422e3f076a3350
Android2021 <jezreldavesondia@gmail.com>
2021-11-29 07:09:51 +0800

    improve notification

--- routes/notification.js
+++ routes/notification.js
@@ -8,27 +8,102 @@ const router = express.Router();
 //initialize fs
 const fs = require('fs');
 
+const list_of_symbols = [
+    "GOLD",
+    "XAUUSD",
+    "AUDCAD",
+    "AUDCHF",
+    "AUDJPY",
+    "AUDNZD",
+    "AUDUSD",
+    "CADCHF",
+    "CADJPY",
+    "CHFJPY",
+    "EURAUD",
+    "EURCAD",
+    "EURCHF",
+    "EURGBP",
+    "EURJPY",
+    "EURNZD",
+    "EURUSD",
+    "GBPAUD",
+    "GBPCAD",
+    "GBPCHF",
+    "GBPJPY",
+    "GBPUSD",
+    "NZDCAD",
+    "NZDJPY",
+    "NZDUSD",
+    "USDCAD",
+    "USDCHF",
+    "USDJPY",
+    "XAUUSD",
+]
+
 
 router.get('/', (req, res) => {
-    fs.readFile('./db/notification.txt', (err, data) => {
+    fs.readFile('./db/notification.json', (err, data) => {
+        let notifications = JSON.parse(data)
         if (err) {
-            res.send(err);
+            return res.send(err);
         } else {
-            res.send(data.toString());
+            return res.send(notifications);
         }
     });
 })
 
 router.post('/', (req, res) => {
     var notification = req.body.notification;
-    fs.writeFile('./db/notification.txt', notification, (err) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send('success');
-        }
-    });
+    fs.readFile('./db/notification.json', (err, data) => {
+        let notificationList
+        let hasSymbol = false
+        let hasDriection = false
+        let hasPrices = false
 
+
+        notificationList = JSON.parse(data)
+
+        for (var i = 0; i < list_of_symbols.length; i++) {
+            var tempSymbol = list_of_symbols[i];
+
+            //tempsymbol to lower case
+            tempSymbol = tempSymbol.toLowerCase();
+
+            //if tempSymbol is present in the sentence, set symbol to tempSymbol
+            if (notification.toLowerCase().includes(tempSymbol)) {
+                hasSymbol = true
+            }
+        }
+
+        if (notification.toLowerCase().includes("buy") || notification.toLowerCase().includes("sell")) {
+            hasDirection = true
+        }
+
+        var numbers = notification.match(/\d+\.?\d*/g);
+
+        console.log(numbers)
+
+        if (numbers.length >= 3) {
+            hasPrices = true
+        }
+
+        console.log(hasPrices)
+
+        console.log(notificationList)
+
+        if (hasSymbol && hasDirection && hasPrices) {
+            notificationList.push(notification)
+        } else {
+            console.log("Not a Signal")
+        }
+
+        fs.writeFile('./db/notification.json', JSON.stringify(notificationList), (err) => {
+            if (err) {
+                console.log(err)
+            }
+            return res.send(notificationList)
+        })
+    })
 })
 
 
